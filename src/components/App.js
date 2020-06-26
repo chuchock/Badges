265767e6ef117428bd92a4ecd811ed4a4cd068d3
Jesús Montero <jesusmontero.developer@gmail.com>
2020-06-26 14:00:11 -0500

    Added BrowserRouter and Layout to manage Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './styles/App.css';
 
-import Navbar from './Navbar';
+import Layout from './Layout';
 import Home from '../pages/Home';
-import { BrowserRouter } from 'react-router-dom';
+import Badges from '../pages/Badges';
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Navbar />
-			<Home />
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/badges" component={Badges} />
+					{/* <Route exact path="/badges/new" component={BadgeNew} />
+					<Route component={NotFound} /> */}
+				</Switch>
+			</Layout>
 		</BrowserRouter>
 	);
 }
