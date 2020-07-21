a2c30cdbe3c0a0a6e77dc2d5faa4adada3cc6d41
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-21 18:51:04 -0500

    BadgeDetails component divided into BadgeDetailsContainer.js and BadgeDetails.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,10 @@ import Layout from './Layout';
 import Home from '../pages/Home';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
-import BadgeDetails from '../pages/BadgeDetails';
+import BadgeDetailsContainer from '../pages/BadgeDetailsContainer';
 import BadgeEdit from '../pages/BadgeEdit';
 import NotFound from '../pages/NotFound';
+
 
 function App() {
 	return (
@@ -19,7 +20,7 @@ function App() {
 					<Route exact path="/" component={Home} />
 					<Route exact path="/badges" component={Badges} />
 					<Route exact path="/badges/new" component={BadgeNew} />
-					<Route exact path="/badges/:badgeId" component={BadgeDetails} />
+					<Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
 					<Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
 					<Route component={NotFound} />
 				</Switch>
